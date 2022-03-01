dc7ba13804c6751f8aa130760c8bd04df1bd44cc
mellyLUCAS <melly.lucas@ynov.com>
2022-03-01 19:36:45 +0100

    test3

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+app.get('/jokes/random', (req, res) => {
+  request(
+    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
 
 app.use(bodyParser.json());
 
